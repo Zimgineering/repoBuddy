eaa61756e6a12bb8cb324d26b4506799a3e8991b
Zimgineer <49336941+Zimgineering@users.noreply.github.com>
2023-04-19 20:33:06 -0700

    add watchdog handling for RB not responding

--- watchdog.bat
+++ watchdog.bat
@@ -1,13 +1,15 @@
 @echo off
 set RB_PID=%1
-set FFIV_PID=%2
+set FFXIV_PID=%2
 
 title [repoBuddy] WatchDog
-echo [repoBuddy] waiting for RB (PID: %1, attached to FFXIV PID: %2) to close before restarting...
+echo [repoBuddy] waiting for RB (PID: %RB_PID%, attached to FFXIV PID: %FFXIV_PID%) to close before restarting...
 
 :whileRB
-TASKLIST /FI "PID eq %1" | FINDSTR /I "RebornBuddy.exe" >nul && goto :whileRB
+timeout /t 5 /nobreak
+taskkill /PID %RB_PID% /F /FI "status eq not responding" >nul
+TASKLIST /FI "PID eq %RB_PID%" | FINDSTR %RB_PID% >nul && goto :whileRB
 
-start rebornbuddy.exe --processid=%2 -a
+start rebornbuddy.exe --processid=%FFXIV_PID% -a
 
 exit
